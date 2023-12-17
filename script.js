c11b1f680e0440c23ce63f381d2083e66d4f7c79
JK1119 <jiho.5640@gmail.com>
2023-12-18 04:09:44 +0900

    first build test

---
+++ script.js
@@ -0,0 +1,11 @@
+// var links = document.querySelectorAll('.project-titles a');
+// var images = document.querySelectorAll('.image img');
+
+// links.forEach(function(link, index) {
+//  link.addEventListener('mouseover', function() {
+//     images.forEach(function(image) {
+//       image.style.opacity = '0';
+//     });
+//     images[index].style.opacity = '100%';
+//  });
+// });
